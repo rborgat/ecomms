5dfae87b64a1bb28626c5fa80bf47b588ef397d8
rborgat <rborgat2018@icloud.com>
2021-07-01 12:00:51 -0400

    a

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -37,8 +37,8 @@ const createNewOrder = catchAsync(async (session, req) => {
 
   const order = await Order.create({
     user: "60d613e99e7c5f072f2145d9",
-    products: sessions.session.ids,
-    shippingAddress: sessions.session.shippingAddress,
+    products: ["60d613e99e7c5f072f2145d9"],
+    shippingAddress: sessions,
     total: 234,
     headers: session.client_reference_id,
   });
