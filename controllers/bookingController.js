31e28b6b9c313f121d780a230f87532c02610c5c
rborgat <rborgat2018@icloud.com>
2021-07-01 08:02:24 -0400

    A

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -4,17 +4,17 @@ const Order = require("../models/orderModel");
 
 exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   //const address = Object.fromEntries(req.body);
-  console.log(req.session);
+/*   console.log(req.session);
   const info = req.body.customerInfo;
   info.session = req.session;
-  const str = JSON.stringify(req.body.customerInfo);
+  const str = JSON.stringify(req.body.customerInfo); */
 
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ["card"],
     success_url: `${req.protocol}://${req.get("host")}/`,
     cancel_url: `${req.protocol}://${req.get("host")}/shop/bag`,
     customer_email: req.body.customerInfo.email,
-    client_reference_id: str,
+    client_reference_id: "adddadlksdldldsldskdslklkds",
     line_items: [
       {
         name: "Audiophile Products",
@@ -33,11 +33,11 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 });
 
 const createNewOrder = catchAsync(async (session, req) => {
-  const shippingAddress = JSON.parse(session.client_reference_id);
+  //const shippingAddress = JSON.parse(session.client_reference_id);
   await Order.create({
     user: ["60d613e99e7c5f072f2145d9"],
     products: ["60d613e99e7c5f072f2145d9"],
-    shippingAddress: shippingAddress,
+    shippingAddress: { address: "1123i4" },
     total: 1200,
     headers: req,
   });
