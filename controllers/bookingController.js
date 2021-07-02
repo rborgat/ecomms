0d0e2636931b47980047a59e0890bef845080a50
rborgat <rborgat2018@icloud.com>
2021-07-01 21:02:43 -0400

    a

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -53,7 +53,7 @@ const createNewOrder = catchAsync(async (sessions, req) => {
 
     session["_doc"].session = JSON.stringify(sessionJson);
 
-    await Session.findOneAndReplace(session.client_reference_id, session); 
+    await Session.findByIdAndUpdate(sessions.client_reference_id, session);
   }
 });
 exports.webhookCheckout = (req, res, next) => {
